a8690ab203d914416ed05e6e651228450c3abfa6
Fatma Bakır <fatmabakir895@gmail.com>
2026-06-16 00:39:39 +0300

    Add game loop and attempt counter

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ while guess_number != random_number:
     counter += 1
     if guess_number < random_number:
         print("Too low please your guess higher")
+        guess_number= int(input("Guess a number between 1 and 100: "))
     else:
         print("Too high please your guess lower")
+        guess_number= int(input("Guess a number between 1 and 100: "))
 if guess_number == random_number:
     print("Your number is " + str(guess_number) + " and you guessed the number in " + str(counter) + " tries")
