c17f18b1ca3227f7fcc7ac049a328af32c39d9ef
Fatma Bakır <fatmabakir895@gmail.com>
2026-06-15 23:56:45 +0300

    Add basic number guess game

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+random_number=random.randint(1, 100);
+counter = 0
+guess_number= int(input("Guess a number between 1 and 100: "))
+#kaç tane tahmin var bilmiyorum for kullanmıcam while daha mantıklı
+while guess_number != random_number:
+    counter += 1
+    if guess_number < random_number:
+        print("Too low please your guess higher")
+    else:
+        print("Too high please your guess lower")
+if guess_number == random_number:
+    print("Your number is " + str(guess_number) + " and you guessed the number in " + str(counter) + " tries")
